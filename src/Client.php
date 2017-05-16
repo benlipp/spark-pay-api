581e81f28acf645d1d92028d619ec02968b0171d
Ben Lippincott <benji.lippincott@gmail.com>
2017-05-16 12:04:08 -0400

    added client auth

--- src/Client.php
+++ src/Client.php
@@ -2,11 +2,17 @@
 
 namespace Benlipp\SparkPay;
 
+use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\HandlerStack;
+use Psr\Http\Message\RequestInterface;
+
 class Client
 {
 
-
-    //protected $httpClient;
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $httpClient;
 
     /**
      * @var string
@@ -26,8 +32,42 @@ class Client
      */
     public function __construct($url, $apiKey)
     {
-        $this->url = $url;
+        $this->url = self::formatUrl($url);
         $this->apiKey = $apiKey;
+        $stack = new HandlerStack();
+        $stack->push($this->authHandler());
+        $this->httpClient = new HttpClient([
+            'handler'  => $stack,
+            'base_uri' => $this->url,
+            'timeout'  => 5,
+        ]);
+    }
+
+    /**
+     * Auth handler for Client
+     * @return \Closure
+     */
+    private function authHandler()
+    {
+        return function (callable $handler) {
+            return function (RequestInterface $request, array $options) use ($handler) {
+                $request = $request->withHeader('X-AC-Auth-Token', $this->apiKey);
+
+                return $handler($request, $options);
+            };
+        };
+    }
+
+    /**
+     * Format the provided store url into the API url.
+     * @param $url
+     * @return string
+     */
+    private static function formatUrl($url)
+    {
+        $url = rtrim($url, "/");
+
+        return "$url/api/v1/";
     }
 
 }
